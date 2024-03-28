c109fb222687a0676f7ab93928a08304837ffe67
Yvisom Manoel <yvisom2012@gmail.com>
2024-03-28 20:28:56 -0300

    Aula php functions

---
+++ aula04/ex1.php
@@ -0,0 +1,10 @@
+<?php 
+ function soma($a, $b) {
+    $z = $a + $b;
+    return $z;
+  }
+  
+  echo "5 + 10 = " . soma(5, 10) . "<br>";
+  echo "20 + 30 =" . soma(20, 30) ."<br>";
+  
+?>
---
+++ aula04/functions.php
@@ -0,0 +1,18 @@
+<?php
+    function Mensagem(){
+        echo "To sem ideia <br>";
+    }
+
+    Mensagem();
+
+    function familyName ($fname){
+        echo "$fname Siles. <br>";
+
+    }
+
+    familyName("João");
+    familyName("Lais");
+    familyName("Bug");
+
+    
+?>
